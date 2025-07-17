0bef5270c7dc80b213841f13629b5d8a081d85e0
지원 <won_8014@naver.com>
2025-07-17 17:29:11 +0900

    inputAction, cinemachinecamera
    인풋액션 unity invoke로 바꾸었고
    시네머신 카메라와 player 오브젝트를 정리하였습니다.

--- Assets/Script/PlayerContoller.cs
+++ Assets/Script/PlayerContoller.cs
@@ -5,16 +5,33 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody rb;
 
-    public void OnMove(InputValue value)
+    void Awake()
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log($"OnMove 호출됨: {moveInput}"); // 디버깅용
+        rb=GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        // 3D 이동: x축, z축만 사용 (y축 고정)
-        transform.Translate(moveInput.x * moveSpeed * Time.deltaTime, 0, moveInput.y * moveSpeed * Time.deltaTime);
+        HandleMovement();
     }
+
+
+    //입력 일어날 때마다 Unity가 자동 생성, 전달해주는 정보
+    public void Move(InputAction.CallbackContext context)//context = 입력 정보
+    {
+        moveInput =  context.ReadValue<Vector2>();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 currentPosition = rb.position;//현재 위치
+        Vector3 Movement = new Vector3(moveInput.x, 0, moveInput.y);//움직인 정도
+
+        Vector3 newPosition = currentPosition + Movement * moveSpeed * Time.fixedDeltaTime;//현재 + 움직인정도
+        rb.MovePosition(newPosition);//움직이기
+
+    }
+
 }
